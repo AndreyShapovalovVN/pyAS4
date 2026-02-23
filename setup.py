a0bea7d807132b5b8d690a909c8d344d4e240b71
Andrey <a147@ukr.net>
2026-02-23 19:02:29 +0100

    Add publication to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyAS4',
-    version='7',
+    version='8',
     license='MIT',
     author='Andrii Shapovalov',
     author_email='mt.andrey@gmail.com',
